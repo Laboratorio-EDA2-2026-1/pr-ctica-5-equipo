9683eaa67b4ed65796a03ea3a0a622b3d1bafbd1
Jorge Ariel Navarro Rodríguez <jorgeariel834@gmail.com>
2025-09-26 08:21:03 -0600

    Update busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -56,3 +56,36 @@ int main(){
         printf("elemento %d no ha sido encontrado");
     }
 }
+
+//EJERCICIO 3
+
+int B_lineal_Centinela(int A[], int x, int tamaño){ 
+    int ix=-1;
+    int temp = A[tamaño-1];
+    A[tamaño-1] = x;
+    int k=0;
+    while (A[k]!=x)
+    {
+        k++;
+    }
+
+    A[tamaño-1] = temp;
+    if (k < tamaño - 1 || A[tamaño - 1] == x) {
+        return k;
+    }
+
+    return ix;
+}
+
+int main(){
+    int A[] = {4,1,3,7,5};
+    int encont = 3;
+    int tamaño = sizeof(A) / sizeof(A[0]);
+    int r = B_lineal_Centinela(A,encont,tamaño);
+    if (r>-1){
+        printf("elemento %d encontrado en la posición: [%d]", encont, r);
+        }
+    else{
+        printf("elemento %d no ha sido encontrado");
+    }
+}
