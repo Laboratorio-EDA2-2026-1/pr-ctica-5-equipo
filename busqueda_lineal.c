ffdce1aa1e12bc326b592902e37d186290a9dff0
Jorge Ariel Navarro Rodríguez <jorgeariel834@gmail.com>
2025-09-26 08:09:22 -0600

    Update busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -1,8 +1,37 @@
 // Escribe aquí la implementación de los ejercicios de búsqueda lineal.
 
-//EJERCICIO 1
+//EJERCICIO 1 Busqueda Lineal
 
 int B_lineal(int A[], int x, int tamaño){ 
+    int ix = -1;
+    int encontrado;
+    
+    for(int i=0; i< tamaño; i++){
+         if (A[i] == x) {
+            encontrado = i;  
+        }
+    }
+    return encontrado;
+
+}
+
+
+int main(){
+    int A[] = {4,1,3,7,5};
+    int encont = 3;
+    int tamaño = sizeof(A) / sizeof(A[0]);
+    int r = B_lineal(A,encont,tamaño);
+    if (r>-1){
+        printf("elemento %d encontrado en la posición: [%d]", encont, r);
+        }
+    else{
+        printf("elemento %d no ha sido encontrado");
+    }
+}
+
+//EJERCICIO 2 Busqueda Lineal "mejorada"
+
+int B_linealM(int A[], int x, int tamaño){ 
     int ix = -1;
     
     for(int i=0; i< tamaño; i++){
@@ -19,7 +48,7 @@ int main(){
     int A[] = {4,1,3,7,5};
     int encont = 3;
     int tamaño = sizeof(A) / sizeof(A[0]);
-    int r = B_lineal(A,encont,tamaño);
+    int r = B_linealM(A,encont,tamaño);
     if (r>-1){
         printf("elemento %d encontrado en la posición: [%d]", encont, r);
         }
